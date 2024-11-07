0e4449fc67fb3b228e27083b3716dcfc8cda472e
Noela-evie <noelanalugwa@gmail.com>
2024-11-07 18:53:49 +0300

    fourth commit

--- backend/src/mongo/index.js
+++ backend/src/mongo/index.js
@@ -6,8 +6,11 @@ dotenv.config();
 export const dbConnect = () => {
   mongoose.set('strictQuery', true);
   mongoose.connection.once("open", () => console.log("DB connection"));
-  return mongoose.connect(
-    `${process.env.DB_LINK}?retryWrites=true&w=majority`,
-    { keepAlive: true }
-  );
-};
+  return mongoose.connect(`${process.env.DB_LINK}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    keepAlive: true,
+    retryWrites: true,
+    w: 'majority',
+  });
+}
