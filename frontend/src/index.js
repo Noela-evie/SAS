8d9499ffb8b9c7d35d6e14cbe9bcc0a907ca7265
Noela <noelanalugwa@gmail.com>
2024-11-11 04:37:40 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "App";
 import { AuthContextProvider } from "context";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+
 
 
 // Material Dashboard 2 React Context Provider
