b217e22423c4494d8bd1d6ff338b6e4e067092f4
Noela-evie <noelanalugwa@gmail.com>
2024-11-11 19:10:05 +0300

    Revert "Update index.js"
    This reverts commit 8d9499ffb8b9c7d35d6e14cbe9bcc0a907ca7265.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,8 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "App";
 import { AuthContextProvider } from "context";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 
 // Material Dashboard 2 React Context Provider
