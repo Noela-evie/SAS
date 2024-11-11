1f10bc34a52f98b988bf88804a58bb5bd41df33a
Noela-evie <noelanalugwa@gmail.com>
2024-11-11 19:03:31 +0300

    Tenth commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ import ReseedAction from "./mongo/ReseedAction.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
 
